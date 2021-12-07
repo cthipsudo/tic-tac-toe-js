a98ce036af6efe9a07e071e35a95c1af7291b20e
cthipsudo <chanpasong.thipphakhinkeo@gmail.com>
2021-12-07 15:52:58 -0600

    Created gameboard logic and player factory function

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log('Hello World');
+
+// Singular Module
+const GameBoard = (() => {
+  let turnOrder = 0;
+  const win = () => console.log('You won!');
+  const lose = () => console.log('You lose');
+  const nextTurn = () => turnOrder++; 
+  const showTurnOrder = () => turnOrder;
+  return {
+    win,
+    lose,
+    nextTurn,
+    showTurnOrder,
+  }; 
+})();
+
+// Factory function
+const playerFactory = (symbol, score) => {
+  const sayHi = () => console.log('Hi');
+  return {symbol, score, sayHi};
+}
+
+const playerOne = playerFactory('x', 0);
+
