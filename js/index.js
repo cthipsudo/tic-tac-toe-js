3b2486a395e79a1243708b830cb178c41b0d09b9
cthipsudo <chanpasong.thipphakhinkeo@gmail.com>
2021-12-08 12:30:09 -0600

    Completed Click listeners for game block

--- js/index.js
+++ js/index.js
@@ -7,20 +7,47 @@ const GameBoard = (() => {
   const lose = () => console.log("You lose");
   const nextTurn = () => turnOrder++;
   const showTurnOrder = () => turnOrder;
+  const consoleGrid = () => console.log(gameGrid);
   const generateGame = () => {
     for (let i = 0; i < 9; i++) {
-      let symbol = i%2 != 0 ? 'x' : 'o';
-      const block = blockFactory(symbol, i, true);
+      let symbol = ""; //i%2 != 0 ? 'x' : 'o';
+      const block = blockFactory(symbol, i, false);
       gameGrid.push(block);
     }
-    gameGrid.forEach(block => {
+    gameGrid.forEach((block) => {
       const blockElement = document.createElement("div");
-      const blockContent = document.createElement('p');
-      blockContent.setAttribute('data-index', block.index);
+      const blockContent = document.createElement("p");
+      blockElement.setAttribute("data-index", block.index);
       blockContent.textContent = block.symbol;
       blockElement.append(blockContent);
       gridElement.append(blockElement);
     });
+    addBlocklisteners();
+  };
+
+  const changeBlock = (index) => {
+    const gameBlock = gameGrid[index];
+    if (!gameBlock.placedOrNot) {
+      // if freespace
+      gameBlock.symbol = turnOrder % 2 != 0 ? "x" : "o";
+      gameBlock.placedOrNot = true;
+      updateTurn();
+    }
+    return gameBlock.symbol;
+  };
+
+  const addBlocklisteners = () => {
+    const blocks = document.querySelectorAll(".game-board div");
+    blocks.forEach((block) => {
+      block.addEventListener("click", () => {
+        const index = block.getAttribute("data-index");
+        block.textContent = changeBlock(index);
+      });
+    });
+  };
+
+  const updateTurn = () => {
+    turnOrder++;
   };
   return {
     win,
@@ -28,16 +55,17 @@ const GameBoard = (() => {
     nextTurn,
     showTurnOrder,
     generateGame,
+    consoleGrid,
   };
 })();
 
 const blockFactory = (symbol, index, placedOrNot) => {
-  return{
+  return {
     symbol,
     index,
-    placedOrNot
-  }
-}
+    placedOrNot,
+  };
+};
 
 // Factory function
 const playerFactory = (symbol, score) => {
@@ -47,9 +75,9 @@ const playerFactory = (symbol, score) => {
 
 const playerOne = playerFactory("x", 0);
 
-
 const main = () => {
   GameBoard.generateGame();
-}
+  GameBoard.consoleGrid();
+};
 
 main();
