57fd8c1bb3be48d150b654bbfec6ef6e04d62d18
cthipsudo <chanpasong.thipphakhinkeo@gmail.com>
2021-12-08 14:15:08 -0600

    Created Win function that checks for row, col, and cross wins

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 // Singular Module
 const GameBoard = (() => {
   const gridElement = document.querySelector(".game-board");
+  const turnElement = document.querySelector("#turn-count");
+  const playerElement = document.querySelector("#current-player");
   let turnOrder = 0;
+  let gameOver = false;
   const gameGrid = [];
   const win = () => console.log("You won!");
   const lose = () => console.log("You lose");
@@ -27,7 +30,7 @@ const GameBoard = (() => {
 
   const changeBlock = (index) => {
     const gameBlock = gameGrid[index];
-    if (!gameBlock.placedOrNot) {
+    if (!gameBlock.placedOrNot && !gameOver) {
       // if freespace
       gameBlock.symbol = turnOrder % 2 != 0 ? "x" : "o";
       gameBlock.placedOrNot = true;
@@ -47,11 +50,75 @@ const GameBoard = (() => {
   };
 
   const updateTurn = () => {
-    turnOrder++;
-    const turnElement = document.querySelector('#turn-count');
-    const playerElement = document.querySelector('#current-player');
+    checkForWin();
+    nextTurn();
     turnElement.textContent = turnOrder;
-    playerElement.textContent = turnOrder % 2 != 0 ? '2' : '1';
+    playerElement.textContent = turnOrder % 2 != 0 ? "2" : "1";
+  };
+
+  const checkForWin = () => {
+    loop: for (let i = 1; i <= 9; i++) {
+      block = gameGrid[i - 1];
+      currentSymbol = block.symbol;
+
+      if (currentSymbol != "") {
+        checkRowWin(i, currentSymbol);
+        checkColWin(i, currentSymbol);
+        // Check Cross
+        checkCrossWin(i, currentSymbol);
+      } 
+    }
+  };
+
+  const checkRowWin = (i, currentSymbol) => {
+    if (i % 3 === 0 && i != 0) {
+      //Check for row matches
+      //console.log('I fire');
+      if (
+        gameGrid[i - 2].symbol === currentSymbol &&
+        gameGrid[i - 3].symbol === currentSymbol
+      ) {
+        winMessage();
+      }
+    }
+  }
+  const checkColWin = (i, currentSymbol) => {
+    // Check Columns
+    if (i <= 3) {
+      if (
+        gameGrid[i + 2].symbol === currentSymbol &&
+        gameGrid[i + 5].symbol === currentSymbol
+      ) {
+        winMessage();
+      }
+    }
+  }
+
+  const checkCrossWin = (i, currentSymbol) => {
+    console.log("I Check");
+    if (i === 5) {
+      console.log(
+        `Comparing ${gameGrid[i - 5].symbol} and ${gameGrid[i + 3].symbol}`
+      );
+      if (
+        gameGrid[i - 5].symbol === currentSymbol &&
+        gameGrid[i + 3].symbol === currentSymbol
+      ) {
+        winMessage();
+      }
+      if (
+        gameGrid[i - 3].symbol === currentSymbol &&
+        gameGrid[i + 1].symbol === currentSymbol
+      ) {
+        winMessage();
+      }
+    }
+  };
+
+  const winMessage = () => {
+    gameOver = true;
+    const gameStatus = document.querySelector("#game-status");
+    gameStatus.textContent = `Player ${turnOrder % 2 != 0 ? "2" : "1"} Wins!`;
   };
   return {
     win,
